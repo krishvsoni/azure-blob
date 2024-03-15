ecd5a6881eb6c9b47b70dc39eccc69c025b3685a
KRISH SONI <67964054+krishvsoni@users.noreply.github.com>
2024-03-15 09:39:51 +0530

    Update view.js

--- view.js
+++ view.js
@@ -7,6 +7,7 @@ const BLOB_NAME = '';
 
 const SAS_TOKEN = '';
 
+
 async function generateBlobUrl() {
   const blobServiceClient = BlobServiceClient.fromConnectionString(AZURE_STORAGE_CONNECTION_STRING);
   const containerClient = blobServiceClient.getContainerClient(CONTAINER_NAME);
